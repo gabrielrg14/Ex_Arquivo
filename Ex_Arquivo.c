58856b7b93c0549d95d4a9d3d71cadcd55322554
gabrielrg14 <43639996+gabrielrg14@users.noreply.github.com>
2018-09-27 07:55:00 -0300

    Add files via upload

---
+++ Ex_Arquivo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	FILE *arq;
+	char nomeArq[100], aux;
+	int tam=0, contL=1;
+	
+	printf("Entre com o nome do arquivo: ");
+	scanf("%s", nomeArq);
+	arq = fopen (nomeArq, "r");
+	
+	if(arq == NULL){
+	printf("Erro ao abrir o arquivo!");
+	return 1;
+	}
+	
+	while(fscanf(arq, "%c", &aux) != EOF){
+		if(aux == '\n')
+			contL++;
+		if(aux != '')	
+	tam ++;
+	}
+	
+	printf("Caracteres no texto: %d\n", tam);
+	printf("Linhas no texto: %d\n", contL);
+	
+	
+	return 0;
+}
